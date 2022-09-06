b6abdf717303da5517e032404362708c30ab3dcd
JoacoR7 <95701320+JoaquinRuizT@users.noreply.github.com>
2022-09-06 18:18:42 -0300

    first commit

---
+++ src/test/java/com/egg/HuertaAgroecologica/HuertaAgroecologicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.HuertaAgroecologica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuertaAgroecologicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
