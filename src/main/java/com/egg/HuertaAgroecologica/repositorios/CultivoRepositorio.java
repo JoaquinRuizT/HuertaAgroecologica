52ac2dbf211ae2a686e76050ee2da3d80da3bc1b
JoacoR7 <95701320+JoaquinRuizT@users.noreply.github.com>
2022-09-07 11:59:27 -0300

    cultivoRepositorio
    - Foto: repositorio y servicio listos
    - Usuario: repositorio listo, servicio comenzado (ver comentarios en método registrar)
    - Excepción personalizada agregada
    - Cultivo: repositorio listo, servicio sin comenzar

--- src/main/java/com/egg/HuertaAgroecologica/repositorios/CultivoRepositorio.java
+++ src/main/java/com/egg/HuertaAgroecologica/repositorios/CultivoRepositorio.java
@@ -2,10 +2,14 @@ package com.egg.HuertaAgroecologica.repositorios;
 
 import com.egg.HuertaAgroecologica.entidades.Cultivo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CultivoRepositorio extends JpaRepository<Cultivo, Long>{
-
+public interface CultivoRepositorio extends JpaRepository<Cultivo, Integer>{
+    
+    @Query("SELECT c FROM Cultivo c WHERE c.nombre = :nombre")
+    public Cultivo buscarPorNombre(@Param("nombre") String nombre);
     
 }
