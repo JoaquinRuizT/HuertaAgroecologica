84c6c65fba35cd914525433f118135c89f8dde1e
Julieta <gamezjulieta@gmail.com>
2022-09-11 15:20:21 -0300

    metodos listar

--- src/main/java/com/egg/HuertaAgroecologica/controladores/CultivoControlador.java
+++ src/main/java/com/egg/HuertaAgroecologica/controladores/CultivoControlador.java
@@ -1,8 +1,11 @@
 package com.egg.HuertaAgroecologica.controladores;
 
+import com.egg.HuertaAgroecologica.entidades.Cultivo;
 import com.egg.HuertaAgroecologica.servicios.CultivoServicio;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,4 +27,17 @@ public class CultivoControlador {
         return null;
     }
 
+    @GetMapping("/frutas")
+        public String frutas (ModelMap modelo){
+        List<Cultivo> listaFrutas = cultivoServicio.listarFrutas();
+            modelo.addAttribute("listaFrutas", listaFrutas);
+            return "frutas.html";
+        }
+        
+     @GetMapping("/verduras")
+        public String verduras (ModelMap modelo){
+        List<Cultivo> listaVerduras = cultivoServicio.listarVegetales();
+            modelo.addAttribute("listaVerduras", listaVerduras);
+            return "verduras.html";
+        }
 }
