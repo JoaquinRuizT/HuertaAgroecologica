c6e40df21ae7dd23f445da3d99f1d6f762b16d78
JoacoR7 <95701320+JoaquinRuizT@users.noreply.github.com>
2022-09-15 12:22:24 -0300

    Validaciones

--- src/main/java/com/egg/HuertaAgroecologica/servicios/UsuarioServicio.java
+++ src/main/java/com/egg/HuertaAgroecologica/servicios/UsuarioServicio.java
@@ -55,17 +55,21 @@ public class UsuarioServicio implements UserDetailsService {
     private void validarUsuario(String nombre, String email, String password, String password2) throws MiExcepcion {
 
         if (nombre.isEmpty() || nombre == null) {
-            throw new MiExcepcion("el nombre no puede ser nulo o estar vacío");
+            throw new MiExcepcion("Debes colocar un nombre");
         }
         if (email.isEmpty() || email == null) {
-            throw new MiExcepcion("el email no puede ser nulo o estar vacio");
+            throw new MiExcepcion("Debes colocar un email");
         }
         if (password.isEmpty() || password == null || password.length() <= 5) {
-            throw new MiExcepcion("La contraseña no puede estar vacía, y debe tener más de 5 dígitos");
+            throw new MiExcepcion("La contraseña debe tener 6 dígitos o más");
         }
 
         if (!password.equals(password2)) {
             throw new MiExcepcion("Las contraseñas ingresadas deben ser iguales");
+        }
+        
+        if(buscarUsuarioPorEmail(email) != null){
+            throw new MiExcepcion("Ya hay un usuario registrado con ese email");
         }
 
     }
@@ -110,6 +114,16 @@ public class UsuarioServicio implements UserDetailsService {
         List<Usuario> usuarios=new ArrayList();
         usuarios= usuarioRepositorio.findAll();
         return usuarios;
+    }
+    
+    public Usuario buscarUsuarioPorEmail(String email){
+        Usuario usuario = usuarioRepositorio.buscarPorEmail(email);
+        if(usuario != null){
+            return usuario;
+        }
+        else{
+            return null;
+        }
     }
 
     @Override
