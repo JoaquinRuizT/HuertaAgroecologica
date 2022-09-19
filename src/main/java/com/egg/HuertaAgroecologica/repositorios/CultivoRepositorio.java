b7d806903f87781108e84686fa5c1b0110b1df81
JoacoR7 <95701320+JoaquinRuizT@users.noreply.github.com>
2022-09-19 14:09:32 -0300

    AdaptacionesMerge4

--- src/main/java/com/egg/HuertaAgroecologica/repositorios/CultivoRepositorio.java
+++ src/main/java/com/egg/HuertaAgroecologica/repositorios/CultivoRepositorio.java
@@ -8,17 +8,17 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CultivoRepositorio extends JpaRepository<Cultivo, String> {
-
+public interface CultivoRepositorio extends JpaRepository<Cultivo, String>{
+    
     @Query("SELECT c FROM Cultivo c WHERE c.nombre = :nombre")
     public Cultivo buscarPorNombre(@Param("nombre") String nombre);
-
+    
     @Query("SELECT c FROM Cultivo c WHERE c.tipoCultivo = :tipoCultivo")
     public List<Cultivo> buscarPorTipoCultivo(@Param("tipoCultivo") String tipoCultivo);
-
+    
     @Query("SELECT c FROM Cultivo c WHERE c.estacion = :estacion")
     public List<Cultivo> buscarPorTipoEstacion(@Param("estacion") String estacion);
-
+    
     @Query("SELECT c FROM Cultivo c WHERE c.usuario.id = :id")
     public List<Cultivo> buscarCultivoPorUsuario(@Param("id") String usuarioId);
     
